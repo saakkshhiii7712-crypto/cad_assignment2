bf7a3cb03d1767a13e742e9a87fa3a58eaaadd2a
Sakshi <saakkshhiii7712@gmail.com>
2026-04-28 13:21:08 +0530

    Add a dictionary and print its elements and count

---
+++ dict.py
@@ -0,0 +1,7 @@
+dict1 = {"name": "Tanvi", "age": 18, "course": "BCA"}
+
+print("Dictionary elements:")
+for key in dict1:
+    print(key, ":", dict1[key])
+
+print("Total items in dictionary:", len(dict1))
